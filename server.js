157413f8f589b96c834ee4447b13ec66fedf5110
saldanha <saldanha@ua.pt>
2023-03-11 19:11:41 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid')
+
+var videoCallId = null;
+
+// Set up PeerJ
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// Provide the video call ID to the client through a route
+app.get('/video-call', (req, res) =>{
+      //Generate a unique ID for the video call
+  videoCallId = generateVideoCallID();
+  res.json({ videoCallId}); // reply with the id of the call
+});
+
+// Connect to a room
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+  })
+})
+
+
+
+server.listen(3000)
+
+
+function generateVideoCallID(){ // Id generator 
+  return uuidv4();
+}
+
+
+
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidV4()}`)
+// })
+
+// app.get('/:room', (req, res) => {
+//   res.render('room', { roomId: req.params.room })
+// })
+
+// io.on('connection', socket => {
+//   socket.on('join-room', (roomId, userId) => {
+//     socket.join(roomId)
+//     socket.to(roomId).broadcast.emit('user-connected', userId)
+
+//     socket.on('disconnect', () => {
+//       socket.to(roomId).broadcast.emit('user-disconnected', userId)
+//     })
+//   })
+// })
+
+// server.listen(3000)
